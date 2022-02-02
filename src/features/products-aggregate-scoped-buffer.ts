33f609f8f88feb0fac4d67cbb2ad52df83dc31bb
Mohamadreza <243572919+rezalwll@users.noreply.github.com>
2022-02-02 10:19:00 +0330

    feat(products): aggregate scoped buffer

---
+++ src/features/products-aggregate-scoped-buffer.ts
@@ -0,0 +1,4 @@
+/** aggregate the scoped buffer from a numeric series. */
+export function aggregateProductsScopedBuffer(values: readonly number[]): number {
+  return values.length ? [...values].sort((a, b) => a - b)[Math.floor(values.length / 2)] : 0;
+}
--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,4 @@ export { resolveFulfillmentScopedBuffer } from './features/fulfillment-resolve-s
 export { deriveInventoryScopedBuffer } from './features/inventory-derive-scoped-buffer';
 export { InventoryDeriveScopedBufferCard } from './components/InventoryDeriveScopedBufferCard';
 export { selectNavigationScopedBuffer } from './features/navigation-select-scoped-buffer';
+export { aggregateProductsScopedBuffer } from './features/products-aggregate-scoped-buffer';
